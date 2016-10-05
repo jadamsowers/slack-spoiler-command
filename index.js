76df1c72f12273f665b6f4d4fe5ea31101ada963
joshvote <Josh.Vote@csiro.au>
2016-10-05 14:54:16 +0800

    Hopefully added bot

--- index.js
+++ index.js
@@ -1,6 +1,83 @@
 var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var Botkit = require('botkit')
+
+var token = process.env.SLACK_TOKEN
+
+var controller = Botkit.slackbot({
+  // reconnect to Slack RTM when connection goes bad
+  retry: Infinity,
+  debug: false
+})
+
+// Assume single team mode if we have a SLACK_TOKEN
+if (token) {
+  console.log('Starting in single-team mode')
+  controller.spawn({
+    token: token,
+    retry: Infinity
+  }).startRTM(function (err, bot, payload) {
+    if (err) {
+      throw new Error(err)
+    }
+
+    console.log('Connected to Slack RTM')
+  })
+// Otherwise assume multi-team mode - setup beep boop resourcer connection
+} else {
+  console.log('Starting in Beep Boop multi-team mode')
+  require('beepboop-botkit').start(controller, { debug: true })
+}
+
+controller.on('bot_channel_join', function (bot, message) {
+  bot.reply(message, "I'm here!")
+})
+
+controller.hears(['hello', 'hi'], ['direct_mention'], function (bot, message) {
+  bot.reply(message, 'Hello.')
+})
+
+controller.hears(['hello', 'hi'], ['direct_message'], function (bot, message) {
+  bot.reply(message, 'Hello.')
+  bot.reply(message, 'It\'s nice to talk to you directly.')
+})
+
+controller.hears('.*', ['mention'], function (bot, message) {
+  bot.reply(message, 'You really do care about me. :heart:')
+})
+
+controller.hears('help', ['direct_message', 'direct_mention'], function (bot, message) {
+  var help = 'I will respond to the following messages: \n' +
+      '`bot hi` for a simple message.\n' +
+      '`bot attachment` to see a Slack attachment message.\n' +
+      '`@<your bot\'s name>` to demonstrate detecting a mention.\n' +
+      '`bot help` to see this again.'
+  bot.reply(message, help)
+})
+
+controller.hears(['attachment'], ['direct_message', 'direct_mention'], function (bot, message) {
+  var text = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.'
+  var attachments = [{
+    fallback: text,
+    pretext: 'We bring bots to life. :sunglasses: :thumbsup:',
+    title: 'Host, deploy and share your bot in seconds.',
+    image_url: 'https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png',
+    title_link: 'https://beepboophq.com/',
+    text: text,
+    color: '#7CD197'
+  }]
+
+  bot.reply(message, {
+    attachments: attachments
+  }, function (err, resp) {
+    console.log(err, resp)
+  })
+})
+
+controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
+  bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
+})
 
 var VERIFY_TOKEN = process.env.SLACK_VERIFY_TOKEN;
 if (!VERIFY_TOKEN) {
@@ -21,9 +98,6 @@ app.route('/spoiler-callback')
     return res.sendStatus(200);
 })
 .post(bodyParser.urlencoded({ extended: true }), function (req, res) {
-    if (req.body.token !== VERIFY_TOKEN) {
-        return res.sendStatus(401);
-    }
     return res.sendStatus(200);
 });
 
@@ -47,10 +121,10 @@ app.route('/spoiler')
     
     var title, spoiler;
     if (req.body.text.indexOf('"') < 0) {
-        title = user + ' just posted a spoiler.';
+        title = '@' + user + ' just posted a spoiler.';
         spoiler = req.body.text;
     } else if (params.length === 1) {
-        title = user + ' just posted a spoiler.';
+        title = '@' + user + ' just posted a spoiler.';
         spoiler = params[0];
     } else {
         title = user + ' ' + params[0];
