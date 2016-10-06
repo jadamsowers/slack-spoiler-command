d9a56a0e2a8fdf50c240dc8dbccc48a714d516f1
joshvote <Josh.Vote@csiro.au>
2016-10-06 15:23:48 +0800

    Acking?

--- index.js
+++ index.js
@@ -36,8 +36,6 @@ controller.on('slash_command', function(bot, message) {
         return;
     }
     
-    bot.replyAcknowledge();
-    
     var user = message.user_name;
     var params = message.text.match(/\w+|"[^"]+"/g); //split our (possibly quoted) params
     if (params.length === 0) {
@@ -78,6 +76,8 @@ controller.on('slash_command', function(bot, message) {
                 }
             }]
         }]
+    }, function() {
+        bot.replyAcknowledge();
     });
 });
 
